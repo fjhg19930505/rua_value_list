81afad00ddd7ea3d5c81d890adf9b0495dad8dde
fjhg19930505 <ziyoulu1993@163.com>
2020-07-25 17:06:54 +0800

    error通用

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod error;
 pub mod obj_id;
 pub mod var;
 pub mod var_list;
@@ -6,3 +7,4 @@ pub mod value;
 pub use var_list::{VarList};
 pub use value::{ValueType, AnyData, ValueData};
 pub use obj_id::ObjId;
+pub use error::*;

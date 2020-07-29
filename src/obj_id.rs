0788c77643657dd179e262fa1bc24727471d81e9
fjhg19930505 <ziyoulu1993@163.com>
2020-07-29 17:11:48 +0800

    ObjId 可以用string转换出来

--- src/obj_id.rs
+++ src/obj_id.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 #[derive(Debug, Copy, Clone)]
 pub struct ObjId {
     ident_: u32,
@@ -17,4 +19,21 @@ impl ObjId {
         ObjId{ident_: 0, serial_: 0}
     }
 
+}
+
+impl From<String> for ObjId {
+    fn from(str_obj: String) -> Self {
+        let arr_obj = str_obj.split("-");
+        let vec = arr_obj.collect::<Vec<&str>>();
+        if vec.len() != 2 {
+            return ObjId::new_null();
+        }
+
+        let ident = u32::from_str(vec[0]).ok().unwrap();
+        let serial = u32::from_str(vec[1]).ok().unwrap();
+        ObjId{
+            ident_: ident,
+            serial_: serial,
+        }
+    }
 }
